b348165c6151b1d46a7e298142b2627d3f973b51
workfolioPH <workfolioph@proton.me>
2026-09-04 22:24:16 +0800

    Update inquiries.js

--- api/inquiries.js
+++ api/inquiries.js
@@ -78,7 +78,7 @@ export default async function handler(req, res) {
       }
 
       // Generate reference code
-      const randomNum = Math.floor(10000 + Math.random() * 90000);
+      const randomNum = Math.floor(100000 + Math.random() * 900000);
       const ref_code = `WF-${randomNum}`;
 
       const newInquiry = {
