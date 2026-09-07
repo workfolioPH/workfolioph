fe5489efdb18a2b98ba01160f3d2f8094544500e
workfolioPH <workfolioph@proton.me>
2026-09-07 19:51:45 +0800

    Export catalog constants and functions from shared module

---
+++ src/lib/catalog.ts
@@ -0,0 +1,10 @@
+export {
+  PACKAGES,
+  ADDONS,
+  CONTACT_METHODS,
+  STATUSES,
+  resolvePackageId,
+  resolveAddonIds,
+  quoteFromSelection,
+  publicCatalog,
+} from '../../shared/catalog.js';
